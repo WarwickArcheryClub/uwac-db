71e7c5223183527af14f29fc86ba61a77633eb08
David Richardson <david@tankski.co.uk>
2015-07-16 11:57:48 +0100

    Added data to events sql file

--- uwac-events.sql
+++ uwac-events.sql
@@ -5,6 +5,24 @@ DROP TABLE IF EXITS "events" CASCADE;
 -- Create table
 
 CREATE TABLE "events" (
-    "id" SERIAL PRIMARY KEY,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL
 );
+
+-- Insert data
+
+INSERT INTO "events" ("id", "name") VALUES
+	(1, "BUCS (BUSA)"),
+	(2, "BUTC"),
+	(3, "BUTTS"),
+	(4, "Club Competition"),
+	(5, "Tour / Isle of Man"),
+	(6, "Practice Session"),
+	(7, "None Given"),
+	(8, "External Competition"),
+	(9, "Competition Session"),
+	(10, "International Competition");
+
+-- Update sequence of primary ke
+
+SELECT setval('bow_types_id_seq', 10); -- Any rows added will have primary key ID of 10+
