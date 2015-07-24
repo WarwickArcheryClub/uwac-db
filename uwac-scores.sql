a58d5314f6160c9685c23b594ddc743e0604a9c9
David Richardson <david@tankski.co.uk>
2015-07-24 11:45:06 +0100

    Added view creation for individual round records

--- uwac-scores.sql
+++ uwac-scores.sql
@@ -38,3 +38,29 @@ CREATE TABLE "scores_queue" (
     "num_xs" INTEGER DEFAULT NULL,
     "date" DATE NOT NULL
 );
+
+-- Create view to calculate individual records
+CREATE VIEW "individual_records" AS
+SELECT (a.first_name || ' ' || a.last_name) AS archer_name, s.score AS score,
+    r.id AS round_id, s.num_golds AS num_golds FROM scores AS s JOIN 
+        (SELECT MAX(score) AS max_score, round_id, category, bow_type FROM 
+            scores JOIN archers ON archer_id=archers.id
+            WHERE archers.gender='M' 
+            GROUP BY round_id, category, bow_type)
+        AS b ON s.score=b.max_score AND s.category=b.category AND
+            s.round_id=b.round_id AND s.bow_type=b.bow_type
+        JOIN archers AS a ON a.id=s.archer_id
+        JOIN rounds AS r ON r.id=s.round_id 
+UNION
+SELECT (a.first_name || ' ' || a.last_name) AS archer_name, s.score AS score,
+    r.id AS round_id, s.num_golds AS num_golds FROM scores AS s JOIN 
+        (SELECT MAX(score) AS max_score, round_id, category, bow_type FROM 
+            scores JOIN archers ON archer_id=archers.id
+            WHERE archers.gender='F' 
+            GROUP BY round_id, category, bow_type)
+        AS b ON s.score=b.max_score AND s.category=b.category AND
+            s.round_id=b.round_id AND s.bow_type=b.bow_type
+        JOIN archers AS a ON a.id=s.archer_id
+        JOIN rounds AS r ON r.id=s.round_id
+ORDER BY score DESC, num_golds DESC;
+
