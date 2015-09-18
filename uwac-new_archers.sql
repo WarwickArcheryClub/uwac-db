0236911587eb5beee10aa63eb57a5daa80d38599
David Richardson <david@tankski.co.uk>
2015-09-18 17:51:08 +0100

    Add new archer table definition

--- uwac-new_archers.sql
+++ uwac-new_archers.sql
@@ -1,11 +1,14 @@
+-- This should be executed after 'uwac-classifications.sql' has been imported, as it contains
+-- the necessary type definition for this table to construct correctly.
+
 -- Drop table
 
 DROP TABLE IF EXISTS "new_archers" CASCADE;
 
--- Create table;
+-- Create table
 
 CREATE TABLE "new_archers" (
-	"id" SERIAL PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
     "first_name" VARCHAR(255) NOT NULL,
     "last_name" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) DEFAULT NULL,
